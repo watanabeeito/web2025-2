0b33ee3faa354f6ee4eae707e698225092b63113
watanabeeito <zm24160@josai.ac.jp>
2025-10-28 10:50:18 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -1,3 +1,3 @@
-var who65='world';function sayhello65(){alert('Hello,'+ who65 +'!');}
-  function taro65(){who65='太郎';}
-function hanako65(){who65='花子';}
+var who65='world';function sayhello66(){alert('Hello,'+ who66 +'!');}
+  function taro65(){who66='太郎が来た';}
+function hanako65(){who66='花子が来た';}
