079ed36c94581c7bb1d15c438c0a11139f331001
watanabeeito <zm24160@josai.ac.jp>
2025-11-17 12:34:28 +0900

    Update ex6-8.js

--- ex6-8.js
+++ ex6-8.js
@@ -1,2 +1,2 @@
-var who67 ='world'; function sayhello67(){alert('Hello,'+who67+'!');}
-function someone(x){who67=x;}
+var who68 ='world'; function sayhello68(){alert('Hello,'+who68+'!');}
+function someone(x){who68=x;}
