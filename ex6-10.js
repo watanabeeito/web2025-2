ec5baa3e10c614737c1591b4723e2ae61d9ee5c2
watanabeeito <zm24160@josai.ac.jp>
2025-11-18 11:11:02 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,1 +1,1 @@
-function sayhello610(){document.getElementById('who610').innerHTML='<img src="./image/huri.22.jpg" alt="オン">';}
+function gazo610(){document.getElementById('who610').innerHTML='<img src="./image/huri.22.jpg" alt="オン">';}
