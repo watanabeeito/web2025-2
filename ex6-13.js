d606cfe946918bd5c59caa1ac34605427a89526e
watanabeeito <zm24160@josai.ac.jp>
2025-12-11 11:49:02 +0900

    Update ex6-13.js

--- ex6-13.js
+++ ex6-13.js
@@ -1,1 +1,2 @@
-function imagechange1(){document.getElementById('logo2').src='./image/城西ロゴ2.pnp';}
+function imagechange1(){document.getElementById('logo2').src='./image/城西ロゴ2.png';}
+function imagechange2(){document.getElementById('logo2').src='./image/城西ロゴ1.jpg';}
