8ab17e3938635b832cee25f148e54e483e83ad50
watanabeeito <zm24160@josai.ac.jp>
2025-12-11 12:04:09 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,1 +1,2 @@
-function imagechange3(){document.getElementById('face').src='./image/
+function imagechange3(){document.getElementById('face').src='./image/wataei2.jpg';}
+function imagechange4(){document.getElementById('face').src='./image/wataei.jpg';}
