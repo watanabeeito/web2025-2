479a39f47433e502d9e4a164b4625bd86be9a72f
watanabeeito <zm24160@josai.ac.jp>
2025-12-16 09:53:18 +0900

    Create ex6-17.js

---
+++ ex6-17.js
@@ -0,0 +1,5 @@
+function hyoji17(){var str17="一文字ずつ表示します。";
+var cnt17=document.timer17.moji17.value.length;
+if(cnt17<11){document.timer17.moji17.value=str17.substr(0,cnt17+1);}else{
+  document.timer17.moji17.value="";}
+  setTimeout("hyoji17()",1000);}
