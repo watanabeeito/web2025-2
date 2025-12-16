c4fb4edeed3cf54054369c183a79b1209e16b8f1
watanabeeito <zm24160@josai.ac.jp>
2025-12-16 10:39:27 +0900

    Update ex6-18.js

--- ex6-18.js
+++ ex6-18.js
@@ -1,2 +1,4 @@
-function imagechange5(){document.getElementById('face').src='./image/huri.22.jpg';}
+function imagechange5(){const img=document.getElementById('face')}
+setTimeout(function(){img.src="huri.22.jpg";},3000);}
+
 
