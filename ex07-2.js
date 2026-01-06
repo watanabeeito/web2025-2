db1e79a4aaeb9fe44f86ae3bdd53a877b7625569
watanabeeito <zm24160@josai.ac.jp>
2026-01-06 09:55:37 +0900

    Update ex07-2.js

--- ex07-2.js
+++ ex07-2.js
@@ -1,4 +1,4 @@
-function add(){var x=document.getElementById('input1').value;
-               var y=document.getElementById('input2').value;
+function add(){var x=Number(document.getElementById('input1').value;)
+               var y=Number(document.getElementById('input2').value;)
                alert(x+y);}
       
