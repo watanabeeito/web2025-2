80f74353f85b944e9f157a5128a919b49b01ee4f
watanabeeito <zm24160@josai.ac.jp>
2026-01-31 15:41:21 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -1,8 +1,10 @@
 function calcBMI(){let height=Number(document.getElementById("height").value);
-                   let height=Number(document.getElementById("height").value);
+                   let weight=Number(document.getElementById("weight").value);
 
 
 let heightMeter=height / 100;
-let bmi=weight/(heightMeter*heightMeter=
+let bmi=weight/(heightMeter*heightMeter);
+
+document.getElementById("result").innerHTML=
                 "あなたのBMIは" +bmi.toFixed(1)+"です";
                   }
